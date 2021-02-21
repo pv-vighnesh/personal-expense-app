d61ae299412701d6c543995e9df40bb133b70e88
PV Vighnesh <pvighnesh.vishnu@gmail.com>
2021-02-21 21:55:06 +0530

    Adding text i/p & using it to calculate total sum

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Personal Expense Application
 
 This is an application that helps the user to keep track of their expense.
 
+1st commit: Adding button, event listeners
+2nd commit: Adding text input field, Using the value from input field to calculate total expense.
--- app.js
+++ app.js
@@ -1,15 +1,26 @@
+// variable to store total expense
+let totalExpense = 0;
+
 // get element from the document
-const element = document.querySelector("#counterBtn");
+const element = document.querySelector("#addBtn");
+
+// on Button click, add input amount to total expense
+function addExpense() {
+
+    // read the value from Input Element
+    const inputElement = document.querySelector("#expenseAmount");
+    const textAmount = inputElement.value;
+
+    // convert the input to number
+    const expense = parseInt(textAmount, 10);
+
+    // add the number to total expense
+    totalExpense = totalExpense + expense;
+}
 
 // add event listener
-element.addEventListener("click", incrementCounter, false);
+element.addEventListener("click", addExpense, false);
+ 
 
-// add a variable that holds the counter value
-let counter = 0;
 
-// function to increment the counter
-function incrementCounter() {
-    counter = counter + 1;
-    console.log(counter);
-}
 
