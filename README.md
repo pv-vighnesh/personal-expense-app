bd03dfa22de229bed6e4faf5cb9d95ba7e62579f
PV Vighnesh <pvighnesh.vishnu@gmail.com>
2021-02-21 22:31:53 +0530

    Printing total expense on DOM

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ This is an application that helps the user to keep track of their expense.
 
 1st commit: Adding button, event listeners
 2nd commit: Adding text input field, Using the value from input field to calculate total expense.
+3rd commit: Printing total expense on DOM
--- app.js
+++ app.js
@@ -4,6 +4,12 @@ let totalExpense = 0;
 // get element from the document
 const element = document.querySelector("#addBtn");
 
+// get the heading element from the DOM
+const headingAmout = document.querySelector("#outputAmount");
+
+//  set the heading element to the value of total expense
+headingAmout.textContent = totalExpense;
+
 // on Button click, add input amount to total expense
 function addExpense() {
 
@@ -16,6 +22,9 @@ function addExpense() {
 
     // add the number to total expense
     totalExpense = totalExpense + expense;
+
+    //  set the heading element to the value of total expense
+    headingAmout.textContent = totalExpense;
 }
 
 // add event listener
