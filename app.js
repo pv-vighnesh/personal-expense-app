a725dc13df2f62c9c6ade7071979afbf0ff91489
PV Vighnesh <pvighnesh.vishnu@gmail.com>
2021-02-24 00:45:22 +0530

    Cleaning code

--- app.js
+++ app.js
@@ -1,63 +1,78 @@
-// get the data from Element
 const inputElement = document.querySelector("#expenseAmount");
-const element = document.querySelector("#addBtn");
 const headingAmout = document.querySelector("#outputAmount");
 const expenseDesc = document.querySelector("#desc")
 const expenseTable = document.querySelector("#expenseTable");
 
-// variable to store total expense
 let totalExpense = 0;
 
-// Array to store all the expenses 
-const allExpense = [];
-
-//  set the heading element to the value of total expense
 headingAmout.textContent = totalExpense;
 
-// on Button click, add input amount to total expense
+const allExpense = [];
+
 function addExpense() {
 
-    // Object to store Amount spent and description
     const expenseItem = {};
-
-    // storing the value of the input element
     const textAmount = inputElement.value;
-
-    // storing the value of Expense description element
     const description = expenseDesc.value;
-
-    // convert the input to number
     const expense = parseInt(textAmount, 10);
 
-    // adding Amount and description to the object
-if (description !== "" && !isNaN(expense) && expense > 0) {
+    if (description !== "" && !isNaN(expense) && expense > 0) {
     expenseItem.amount = expense;
     expenseItem.desc = description;
     expenseItem.moment = new Date();
 
-}
-    // pusing the object into the array
+    totalExpense += expense;
+    updateTotal();
     allExpense.push(expenseItem);
 
-    // add the number to total expense
-    totalExpense = totalExpense + expense;
-
-    //  set the heading element to the value of total expense
-    const displayText = `Total: ${totalExpense}`;
-    headingAmout.textContent = displayText;
-
-    // Display Expense table
     renderNewList(allExpense);
+    inputElement.value = "";
+    expenseDesc.value = "";
+    }
 }
 
-// add event listener
+const element = document.querySelector("#addBtn");
 element.addEventListener("click", addExpense, false);
+document.addEventListener("keypress", function (event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      addExpenseToTotal();
+    }
+});
+
+function getTime(time) {
+    return time.toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    });
+}
+
+function deleteItem(dateValue, amount) {
+    const filteredArr = allExpense.filter(
+        expense => expense.moment.valueOf() !== dateValue
+        );
+    renderNewList(filteredArr);
+    totalExpense -= amount;
+    updateTotal();
+}
+
+function updateTotal() {
+    let updatedTotal = `Total: ${totalExpense}`;
+    headingAmout.textContent = updatedTotal;
+}
+
+function renderNewList(newArray) {
+    const allExpenseHTML = newArray.map(expense => 
+        createList(expense)
+        );
+    const allExpenseHTMLJoined = allExpenseHTML.join("");
+    expenseTable.innerHTML = allExpenseHTMLJoined;
+    allExpense = newArray;
+}
  
 // function to create HTML for Expense table
 function createList ({ desc, amount, moment }) {
     return `
-    <div>
-        <ul class="list-group">
             <li class="list-group-item d-flex justify-content-between">
                 <div class="d-flex flex-column">
                     ${desc}
@@ -70,49 +85,11 @@ function createList ({ desc, amount, moment }) {
                 <button 
                     type="button" 
                     class="btn btn-outline-danger btn-sm"
-                    onclick="deleteItem(${moment.valueOf()})"
+                    onclick="deleteItem(${moment.valueOf()}, ${amount})"
                     >
                     <i class="fas fa-trash-alt"></i>
                 </button>
                 </div>
             </li>
-        </ul>
-    </div>
-    `
-}
-
-// funtion to get current date
-function getTime(time) {
-    return time.toLocaleDateString('en-US', {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric'
-    });
-}
-
-// function to delete item from Expense table
-function deleteItem(dateValue) {
-    // const filteredArr = [];
-    // for (let i = 0; i < allExpense.length; i++) {
-    //     if (allExpense[i].moment.valueOf() === dateValue) {
-    //         filteredArr.push(allExpense[i]);
-    //     }
-    // }
-
-    // const filteredArr = allExpense.filter(expense => {
-    //     if(expense.moment.valueOf() !== dateValue) {
-    //         return expense;
-    //     }
-    // })
-
-    const filteredArr = allExpense
-                        .filter(expense => expense.moment.valueOf() !== dateValue)
-    renderNewList(filteredArr);
-}
-
-
-function renderNewList(newArray) {
-    const allExpenseHTML = newArray.map(expense => createList(expense));
-    const allExpenseHTMLJoined = allExpenseHTML.join("");
-    expenseTable.innerHTML = allExpenseHTMLJoined;
+        `;
 }
